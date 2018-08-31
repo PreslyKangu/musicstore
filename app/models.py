a14efa4e57eaa6343a0796e0ffd167ee697f18c1
PreslyKangu <preskanjr@gmail.com>
2018-08-31 18:07:04 +0300

    Made changes to admin.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+	"""docstring for Halflondon"""
+	artist = models.CharField(max_length=250)
+	album_title = models.CharField(max_length=500)
+	genre = models.CharField(max_length=1025)
+	album_logo = models.CharField(max_length=1000)
+
+	def __unicode__(self):
+		return self.artist
+
+
+class Song(models.Model):
+	album = models.ForeignKey(Album, on_delete =models.CASCADE)
+	file_type = models.CharField(max_length=150)
+	song_type =models.CharField(max_length=500)
+
+
+	def __unicode__(self):
+		return self.album
--- app/views.py
+++ app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from app.models import Album,Song
 
 # Create your views here.
 def home(request):
+
 	return render(request,'home.html')
+
+
+def Album(request):
+	song  = Song.objects.all()
+	#album = Album.objects.all()
+	return render (request,'Album.html', {'Song':song})
+"""
+,'Album':album
+def (request):
+	song  = Song.Objects.all()
+	album = Album.objects.all()
+	return render (request,'Album.html', {'Song':song,'Album':album})
+"""
+	
